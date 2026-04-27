7c2623dee542cafd8bb215ddeece8f8058362a8e
ipulkitsingla <156352781+ipulkitsingla@users.noreply.github.com>
2026-04-27 22:58:32 +0530

    ..

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://youtube-analytics-pro.onrender.com/api';
 
 // UI Elements
 const loginView = document.getElementById('loginView');
